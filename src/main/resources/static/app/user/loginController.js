5e5553243813f35c6b9df02e1b61d0cbd85828f7
Rafael Albuquerque <rafael.sousa@splab.ufcg.edu.br>
2017-04-08 20:10:17 -0300

    Fix ide related issues

--- src/main/resources/static/app/user/loginController.js
+++ src/main/resources/static/app/user/loginController.js
@@ -2,13 +2,12 @@ angular.module("smallapp").controller("loginController", ['userService', '$scope
 	$scope.user = {}
 	// method for login
 	$scope.login = function() {
-		// requesting the token by usename and passoword
+		// trying to do the authentication
 		userService.login($scope.user.mail, $scope.user.password).then(function(res) {
 				$scope.password = null;		
-				// checking if the token is available in the response
+				// saving the user on the service
 				userService.authUser(res)
 			}, function errorCallback(response)  {
-				console.log(response)
 				$scope.messages = []
 				// if authentication was not successful. Setting the error message.
 				$scope.messages.push(response.data.error)
